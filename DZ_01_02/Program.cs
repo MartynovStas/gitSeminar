8176b85230bab281b84a6e58de78132322b1f043
Stas Martynov <maks.mart71@bk.ru>
2023-02-12 17:16:55 +0400

    закомител домашние задание GB_C#_01

---
+++ DZ_01_02/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+System.Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ведите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ведите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 < number2 && number2 < number3)
+{
+    System.Console.WriteLine("Max = " + number3);
+}
+else if (number1 > number2 && number1 > number3)
+{
+    System.Console.WriteLine("Max = " + number1);
+}
+else if (number1 == number2 && number2 < number3)
+{
+    System.Console.WriteLine("Max = " + number3);
+}
+else if (number2 == number3 && number2 < number1)
+{
+    System.Console.WriteLine("Max = " + number1);
+}
+else
+{
+    System.Console.WriteLine("Max = " + number2);
+}
---
+++ DZ_01_04/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+System.Console.Write("Ведите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int zeloe = 1;
+for(int i = zeloe; i<=number1; i++){
+    if(i %2 == 0){
+        zeloe = i;
+        System.Console.Write(zeloe + " ");
+    }
+}
